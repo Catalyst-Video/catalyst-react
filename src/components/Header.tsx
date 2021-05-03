72e653238e79d1a7f3e6184d79c58feb3427f0fb
Seth <sethgoldin27@gmail.com>
2021-05-03 00:13:21 -0400

    tailwindify header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -48,16 +48,24 @@ const Header = ({
   }
 
   return (
-    <div className="show-top">
-      <div className={alwaysBanner ? 'header-banner' : 'mobile-banner'}>
+    <div>
+      <div
+        className={`absolute left-0 top-0 block w-full text-center bg-${themeColor}-500 p-1 text-white text-sm font-semibold z-4 ${
+          alwaysBanner ? '' : 'sm:hidden'
+        }`}
+      >
         Powered by Catalyst
       </div>
       {!alwaysBanner && (
         <>
-          <div id="ct-header" ref={headerRef} className="ct-not-selectable">
+          <div
+            id="header"
+            ref={headerRef}
+            className="hidden sm:block fixed not-selectable m-3"
+          >
             <button
-              className="ct-header-btn"
-              style={{ display: 'inline' }}
+              id="header-btn"
+              className="focus:border-0 focus:outline-none bg-transparent cursor-pointer inline"
               onClick={() => setShowSessionDetails(!showSessionDetails)}
             >
               <HeaderImg themeColor={themeColor} />
@@ -65,16 +73,18 @@ const Header = ({
           </div>
           {showSessionDetails && (
             <button
-              className="session-details-btn"
+              id="sess-details-btn"
+              className="bg-white rounded-md p-10 z-20 absolute m-5 cursor-pointer shadow-md focus:border-0 focus:outline-none"
               onClick={() => setShowSessionDetails(!showSessionDetails)}
             >
-              <span className="session-details-title">
+              <span id="sess-details-title" className="flex mb-5">
                 <strong>Session Details</strong>
                 <FontAwesomeIcon
                   icon={faTimes}
                   size="lg"
+                  id="sess-details-close"
                   title="Close Session Details"
-                  className="session-details-close"
+                  className="absolute right-1 top-1 cursor-pointer"
                 />
               </span>
               Room:<i> {sessionKey}</i>
