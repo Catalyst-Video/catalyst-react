3c26df619f46ff1f64dc7c62d2d68a0235c3d0e1
Joseph Semrai <josephsemrai@gmail.com>
2021-06-15 20:27:51 -0400

    Update default server address

--- src/utils/globals.ts
+++ src/utils/globals.ts
@@ -1,3 +1,3 @@
-export const DEFAULT_SERVER_ADDRESS = 'https://server.catalyst.chat/';
+export const DEFAULT_SERVER_ADDRESS = 'https://api.catalyst.chat/';
 export const DEFAULT_THEMECOLOR = 'blue';
 export const DEFAULT_AUTOFADE = 600;
