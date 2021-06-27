b9116eeedd148c9f2992ed238af2035aaeba8aa5
Seth <sethgoldin27@gmail.com>
2021-06-27 17:53:28 -0400

    update postcss and tsdx configs

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,6 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
-    // require('tailwindcss/nesting')(require('postcss-nesting')),
     require('tailwindcss'),
     require('autoprefixer'),
     require('cssnano')({
--- tsdx.config.js
+++ tsdx.config.js
@@ -1,19 +1,30 @@
 const postcss = require('rollup-plugin-postcss');
-const autoprefixer = require('autoprefixer');
-const tailwindcss = require('tailwindcss');
-const cssnano = require('cssnano');
 const strip = require('@rollup/plugin-strip');
 // Not transpiled with TypeScript or Babel, so use plain Es6/Node.js
 const replace = require('@rollup/plugin-replace');
-const nesting = require('postcss-nested');
-//require('tailwindcss/nesting');
-// const copy = require('rollup-plugin-copy');
 // const url = require('@rollup/plugin-url');
+// const copy = require('rollup-plugin-copy');
 
 module.exports = {
   rollup(config, opts) {
     config.plugins.push(
-      // url({
+      postcss({
+        config: {
+          path: './postcss.config.js',
+        },
+        extensions: ['.css'],
+        minimize: true,
+        inject: {
+          // Append to <head /> as code running
+          insertAt: 'top',
+          // only write out CSS for the first bundle (avoids pointless extra files): -> make false if we inject true
+        },
+      }),
+      strip({
+        labels: ['unittest'],
+      })
+      // TODO: necessary for audio files
+      //  url({
       //   include: [
       //     '**/*.svg',
       //     '**/*.png',
@@ -24,37 +35,11 @@ module.exports = {
       //   ],
       //   destDir: 'dist/assets',
       // }),
-      postcss({
-        config: {
-          path: './postcss.config.js',
-        },
-        extensions: ['.css'],
-        minimize: true,
-        inject: {
-          insertAt: 'top',
-        },
-      }),
-      /*    plugins: [
-          tailwindcss(),
-          autoprefixer(),
-          nesting(),
-          cssnano({
-            preset: 'default',
-          }),
-        ],
-        // Append to <head /> as code running
-        inject: true, // false
-        // only write out CSS for the first bundle (avoids pointless extra files): -> make false if we inject true
-        extract: false, // !!options.writeMeta,
-      }),
       // copy({
       //   targets: [{ src: 'src/assets/sound', dest: 'dist/assets' }],
       //   copyOnce: true,
       //   verbose: true,
-      // }), */
-      strip({
-        labels: ['unittest'],
-      })
+      // }),
     );
     config.plugins = config.plugins.map(p =>
       p.name === 'replace'
