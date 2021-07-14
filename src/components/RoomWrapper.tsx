30f63fd30f7976345a0701175517d673fcecb110
Seth <sethgoldin27@gmail.com>
2021-07-14 01:06:27 -0400

    try alternative methods of displaying screen share

--- src/components/RoomWrapper.tsx
+++ src/components/RoomWrapper.tsx
@@ -3,6 +3,7 @@ import {
   Participant,
   RemoteVideoTrack,
   Room,
+  TrackPublication,
 } from "livekit-client";
 import { VideoQuality } from "livekit-client/dist/proto/livekit_rtc";
 import React, { ReactElement, Ref, useEffect, useRef, useState } from "react";
@@ -34,20 +35,36 @@ import { debounce } from 'ts-debounce';
      margin: '2px',
    });
 
-   useEffect(() => {
-     window.addEventListener(
-       'load',
-       () => {
-         resizeWrapper();
-         window.onresize = debounce(resizeWrapper, 15);
-       },
-       false
-     );
-   }, []);
-
-   useEffect(() => {
-     resizeWrapper();
-   }, [members, screens]);
+   
+   const resizeWrapper = () => {
+     let margin = 2;
+     let width = 0;
+     let height = 0;
+     if (vidRef.current) {
+       width = vidRef.current.offsetWidth - margin * 2;
+       height = vidRef.current.offsetHeight - margin * 2;
+     }
+     //  console.log('res', width, height);
+     let max = 0;
+     //  TODO: loop needs to be optimized
+     let i = 1;
+     while (i < 5000) {
+       //  TODO: let l = (members.length < 1 ? 1 : members.length) + screens.length;
+       let l = 4 + screens.length;
+       let w = area(i, l, width, height, margin);
+       if (w === false) {
+         max = i - 1;
+         break;
+       }
+       i++;
+     }
+     max = max - margin * 2;
+     setVidDims({
+       width: max + 'px',
+       height: max * 0.5625 + 'px', // 0.5625 enforce 16:9 (vs 0.75 for 4:3)
+       margin: margin + 'px',
+     });
+   };
 
    const area = (
      increment: number,
@@ -71,35 +88,23 @@ import { debounce } from 'ts-debounce';
      else return increment;
    };
 
-   const resizeWrapper = () => {
-     let margin = 2;
-     let width = 0;
-     let height = 0;
-     if (vidRef.current) {
-       width = vidRef.current.offsetWidth - margin * 2;
-       height = vidRef.current.offsetHeight - margin * 2;
-     }
-    //  console.log('res', width, height);
-     let max = 0;
-     //  TODO: loop needs to be optimized
-     let i = 1;
-     while (i < 5000) {
-      //  TODO: let l = (members.length < 1 ? 1 : members.length) + screens.length;
-       let l = 4 + screens.length;
-       let w = area(i, l, width, height, margin);
-       if (w === false) {
-         max = i - 1;
-         break;
-       }
-       i++;
-     }
-     max = max - margin * 2;
-     setVidDims({
-       width: max + 'px',
-       height: max * 0.5625 + 'px', // 0.5625 enforce 16:9 (vs 0.75 for 4:3)
-       margin: margin + 'px',
-     });
-   };
+      const debouncedResize = debounce(resizeWrapper, 15);
+
+      useEffect(() => {
+        window.addEventListener(
+          'load',
+          () => {
+            resizeWrapper();
+            window.onresize = debouncedResize;
+          },
+          false
+        );
+      }, []);
+
+      useEffect(() => {
+        resizeWrapper();
+      }, [members, screens]);
+
 
    if (error || isConnecting || !room || members.length === 0) {
      return (
@@ -113,7 +118,7 @@ import { debounce } from 'ts-debounce';
        </div>
      );
    }
-   // find first participant with screen shared
+  //  // find first participant with screen shared
    let screenTrack: RemoteVideoTrack | undefined;
    members.forEach(p => {
     //  TODO: don't show local screen share if (p instanceof LocalParticipant) {
@@ -130,31 +135,31 @@ import { debounce } from 'ts-debounce';
      });
    });
 
-   let otherParticipants: Participant[];
-   let mainView: ReactElement;
-   if (screenTrack) {
-     otherParticipants = members;
-     mainView = (
-       <ScreenShareView track={screenTrack} height="100%" width="100%" />
-     );
-   } else {
-     otherParticipants = members.slice(1);
-     mainView = (
-       <MemberView
-         key={members[0].identity}
-         member={members[0]}
-         showOverlay={showOverlay}
-         aspectWidth={16}
-         height={vidDims.height}
-         width={vidDims.width}
-         aspectHeight={9}
-         theme={theme}
-         quality={VideoQuality.HIGH}
-         onMouseEnter={() => setShowOverlay(true)}
-         onMouseLeave={() => setShowOverlay(false)}
-       />
-     );
-   }
+  //  let otherParticipants: Participant[];
+  //  let mainView: ReactElement;
+  //  if (screenTrack) {
+  //    otherParticipants = members;
+  //    mainView = (
+  //      <ScreenShareView track={screenTrack} height="100%" width="100%" />
+  //    );
+  //  } else {
+  //    otherParticipants = members.slice(1);
+  //    mainView = (
+  //      <MemberView
+  //        key={members[0].identity}
+  //        member={members[0]}
+  //        showOverlay={showOverlay}
+  //        aspectWidth={16}
+  //        height={vidDims.height}
+  //        width={vidDims.width}
+  //        aspectHeight={9}
+  //        theme={theme}
+  //        quality={VideoQuality.HIGH}
+  //        onMouseEnter={() => setShowOverlay(true)}
+  //        onMouseLeave={() => setShowOverlay(false)}
+  //      />
+  //    );
+  //  }
 
    return (
      <>
@@ -169,23 +174,23 @@ import { debounce } from 'ts-debounce';
          ref={vidRef}
          className={`flex justify-center content-center items-center flex-wrap align-middle z-2 w-full h-full max-h-screen max-w-screen box-border`}
        >
-         {members.map((m, i) => {
-           m.videoTracks.forEach(track => {
+         {/* {members.map((m, i) => {
+           [...m.videoTracks.values()].map((track, i) => {
              if (track.trackName === 'screen' && track.track) {
-               screenTrack = track.track as RemoteVideoTrack;
+               let screenTrack = track.track as RemoteVideoTrack;
                console.log(screenTrack);
-              return (
-                <ScreenShareView
-                  track={screenTrack}
-                  height={vidDims.height}
-                  width={vidDims.width}
-                  key={`${i}-screen`}
-                />
-              );
+               return (
+                 <ScreenShareView
+                   track={screenTrack}
+                   height={vidDims.height}
+                   width={vidDims.width}
+                   key={`${i}-screen`}
+                 />
+               );
              } else return null
            });
-         })}
-         {/* {screens &&
+         })} */}
+         {screens &&
            screens.map((s, i) => {
              return (
                <ScreenShareView
@@ -195,7 +200,7 @@ import { debounce } from 'ts-debounce';
                  key={`${i}-screen`}
                />
              );
-           })} */}
+           })}
          {gridView &&
            [0,1,2,3].map((m, i) => {
              return (
@@ -232,7 +237,7 @@ import { debounce } from 'ts-debounce';
        {!gridView && (
          <div className="grid grid-cols-12 gap-2 z-20 overflow-hidden py-10 px-1">
            {/* auto-rows-min */}
-           <div className="col-start-1 col-end-11">{mainView}</div>
+           {/* <div className="col-start-1 col-end-11">{mainView}</div> */}
            <div className={'sidebar overflow-auto'}>
              {[0, 1, 2, 3].map((participant, i) => {
                let quality = VideoQuality.HIGH;
@@ -255,7 +260,7 @@ import { debounce } from 'ts-debounce';
                  />
                );
              })}
-             {otherParticipants.map((participant, i) => {
+             {/* {otherParticipants.map((participant, i) => {
                let quality = VideoQuality.HIGH;
                if (i > 4) {
                  quality = VideoQuality.LOW;
@@ -275,7 +280,7 @@ import { debounce } from 'ts-debounce';
                    onMouseLeave={() => setShowOverlay(false)}
                  />
                );
-             })}
+             })} */}
            </div>
          </div>
        )}
