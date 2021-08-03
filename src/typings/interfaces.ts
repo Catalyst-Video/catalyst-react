f31e99765077036ff16537e98f3f2d0053db3fd0
Joseph Semrai <josephsemrai@gmail.com>
2021-08-03 01:00:04 -0400

    Add watch script + update interface to reflect optional props

--- src/typings/interfaces.ts
+++ src/typings/interfaces.ts
@@ -39,11 +39,11 @@ export interface CatalystChatProps {
   disableNameField?: boolean;
   cstmSetupBg?: string;
   arbData?: Uint8Array;
-  handleReceiveArbData: (arbData: Uint8Array) => void;
-  onJoinCall: () => void;
-  onMemberJoin: () => void;
-  onMemberLeave: () => void;
-  onLeaveCall: () => void;
+  handleReceiveArbData?: (arbData: Uint8Array) => void;
+  onJoinCall?: () => void;
+  onMemberJoin?: () => void;
+  onMemberLeave?: () => void;
+  onLeaveCall?: () => void;
 }
 
 export interface RoomMetaData {
